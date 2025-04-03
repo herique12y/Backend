c327125d286b281e676c79f78a7c32e81b746f4e
herique12y <wyherique15@gmail.com>
2025-04-03 16:26:30 -0400

    Variaveis e tipos de dados

---
+++ aula-01.js
@@ -0,0 +1,7 @@
+let valor = 13.141516;
+let text = "Meu primeiro projeto em java script";
+let boleano = true;
+
+console.log(valor.toFixed(2));
+console.log(text.toUpperCase());
+console.log(boleano.toString());
