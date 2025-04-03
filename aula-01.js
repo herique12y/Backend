09e4d89da14247b60ccc1dce5d18a292c1fdc41c
herique12y <wyherique15@gmail.com>
2025-04-03 17:40:55 -0400

    exercicio

--- aula-01.js
+++ aula-01.js
@@ -2,6 +2,30 @@ let valor = 13.141516;
 let text = "Meu primeiro projeto em java script";
 let boleano = true;
 
-console.log(valor.toFixed(2));
-console.log(text.toUpperCase());
-console.log(boleano.toString());
+console.log(valor.toFixed(2)); // Arredonda o valor para duas casas decimais
+console.log("Tipo:" + typeof valor); // Retorna o tipo da variável
+console.log(text.toUpperCase()); // Converte o texto para maiúsculas
+console.log("Tipo:" + typeof text); // Retorna o tipo da variável
+console.log(boleano.toString()); // Converte para texto
+console.log("Tipo:" + typeof boleano); // Retorna o tipo da variavel
+
+let estados = ["Salvador", "Rondônia", "Mato Grosso", "São Paulo"];
+let marcaCarros = [];
+marcaCarros.push("Toyota");
+console.log(estados[2]);
+console.log("Tamanho: " + estados.length);
+console.log(marcaCarros[0]);
+console.log("Total de letras: " + marcaCarros[0].length);
+
+let usuario; // variável não definida
+console.log(usuario) //console.log(usuario.length) // TypeError: Cannot read properties of undefined
+
+let idade;
+console.log(idade +1); // NaN - Not a Number
+
+let nula = null;
+//console.log(nula.length);
+console.log(nula + 1);
+
+const aula = "javaScript"; // Definindo uma Constante
+console.log(aula);
---
+++ exercícios.js
@@ -0,0 +1,13 @@
+let nome = "Hérique"
+let idade = "15"
+console.log("Olá, meu nome é " + nome, "e eu tenho " + idade, "anos");
+
+let string = "Homem-Aranha"
+console.log(string.toLowerCase(), string.toUpperCase());
+
+let letra = 23;
+console.log(typeof letra);
+
+let altura = 1.75
+let peso = 65
+console.log(peso /(altura * altura));
