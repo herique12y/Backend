68f16ffbb283fca8d9034fa2c13686985db06dc7
herique12y <wyherique15@gmail.com>
2025-04-10 17:28:46 -0400

    atividade 2

---
+++ exercícios02.js
@@ -0,0 +1,22 @@
+let numero = 5
+let numero2 = 2
+let soma = (numero + numero2);
+let subtracao = (numero - numero2);
+let multiplicacao = (numero * numero2);
+let divisao = (numero / numero2);
+console.log(soma);
+console.log(subtracao);
+console.log(multiplicacao);
+console.log(divisao);
+
+let idade = 25
+let idade2 = 18
+
+let reposta = idade >= idade2 ? "É mais velho" : "É mais novo"
+console.log(reposta)
+
+let Idade = 16
+console.log(Idade >= 18 ? "Maior de Idade" : "Menor de Idade")
+
+let Numero = 14
+console.log(Numero % 2  == 0 ?"Par" : "Impar")
